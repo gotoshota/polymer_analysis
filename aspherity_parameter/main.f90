47c1e61ee17a821f399a04155f3163706fdc76b0
goto-sh-at-Mtbys <goto-sh@cheng.es.osaka-u.ac.jp>
2022-03-08 14:30:44 +0900

    update

--- aspherity_parameter/main.f90
+++ aspherity_parameter/main.f90
@@ -1,7 +1,5 @@
-!This program calculate Aspherity parameter (AS).
-!The algorism is based on following paper.
-!J. Swetlana, et al., "Diffusion- and reaction-limited cluster aggregation revisited"
-!PCCP, 2019.
+!This program calculate Aspherity parameter (AS).  !The algorism is based on following paper.
+!K. Alim and E. Frey, "Shapes of Semiflexible Polymer Rings", PRL (2007).
 program main
     use prm_list
     use read_prm
@@ -11,15 +9,24 @@ program main
     
     implicit none
     DOUBLE PRECISION, ALLOCATABLE :: inertia_tensor(:,:,:,:) !! output as eigenvector 
-    DOUBLE PRECISION tmp(3), tmptmp
-    DOUBLE PRECISION as
-    DOUBLE PRECISION ave(3)
+    DOUBLE PRECISION, ALLOCATABLE :: inertia_tensor_hat(:,:,:,:) !! output as eigenvector 
+    DOUBLE PRECISION, ALLOCATABLE :: inertia_tensor_hat_square(:,:,:,:) !! output as eigenvector 
+    DOUBLE PRECISION tmp(3), tmptmp, tmp_tensor
+    DOUBLE PRECISION ave(2)
+    DOUBLE PRECISION mat(3,3)
     double precision t, dummy, tread, tcalc
     integer h, i, j, k, l, m, n
     integer tbegin, tend, CountPerSec, CountMax
     INTEGER normalize
     
-    
+    DOUBLE PRECISION, ALLOCATABLE :: delta(:,:), sigma(:,:)
+    DOUBLE PRECISION trace, tracetrace
+    DOUBLE PRECISION det
+
+    DOUBLE PRECISION eigenval(3)
+    integer, parameter :: lwork = 10000
+    double precision :: work(lwork)
+    integer :: info
 !############################################################################
 
     call readprm()
@@ -32,6 +39,7 @@ program main
 
     !! calculate inertia ternsor
     ALLOCATE(inertia_tensor(3, 3, nmol, 0:nframe)) !! 1=x, 2=y, 3=z
+    ALLOCATE(inertia_tensor_hat(3, 3, nmol, 0:nframe)) !! 1=x, 2=y, 3=z
     do i = 0, nframe
         do j = 1, nmol 
             do k = 1, natom 
@@ -49,6 +57,53 @@ program main
         enddo
     enddo
     DEALLOCATE(pos, pos_cm)
+    !calculate inertia tensor hat
+    do i = 0, nframe
+        do j = 1, nmol
+            trace = inertia_tensor(1,1,j,i) + inertia_tensor(2,2,j,i) + inertia_tensor(3,3,j,i)
+            trace = trace / 3.0d0
+            do k = 1, 3
+                do l = 1, 3
+                    inertia_tensor_hat(l,k,j,i) = inertia_tensor(l,k,j,i)
+                enddo
+                inertia_tensor_hat(k,k,j,i) = inertia_tensor_hat(k,k,j,i) - trace
+            enddo
+        enddo
+    enddo
+    !! calculate square inertia tensor hat
+    do i = 0, nframe
+        do j = 1, nmol
+            mat(:,:) = inertia_tensor_hat(:,:,j,i)
+            inertia_tensor_hat_square(:,:,j,i) = MATMUL(mat,mat)
+        enddo
+    enddo
+    !! calculate delta
+    ALLOCATE(delta(nmol,0:nframe))
+    do i = 0, nframe
+        do j = 1, nmol
+            !! delta
+            trace = inertia_tensor(1,1,j,i) + inertia_tensor(2,2,j,i) + inertia_tensor(3,3,j,i)
+            tracetrace = inertia_tensor_hat_square(1,1,j,i) + inertia_tensor_hat_square(2,2,j,i) + inertia_tensor_hat_square(3,3,j,i) 
+            delta(j,i) = 1.50d0*tracetrace/trace/trace
+            !! sigma
+            mat = inertia_tensor_hat(:,:,j,i)
+            CALL dsyev('V', 'U', 3, mat, 3, eigenval, work, lwork, info)
+            det = eigenval(1) + eigenval(2) + eigencal(3)
+            tmp = 2.0d0 / 3.0d0 *tracetrace
+            tmp = tmp ** 1.50d0
+            sigma(j,i) = 4.0d0*det / tmp
+        enddo
+    enddo
+
+    !! calculate average
+    ave = 0.0d0
+    do i = 0, nframe
+        do j = 1, nmol
+            ave(1) = ave(1) + delta(j,i)
+            ave(2) = ave(2) + sigma(j,i)
+        enddo
+    enddo
+    ave = ave / DBLE(nframe + 1) / DBLE(nmol)
 
     call system_clock(tend)
     tcalc = real(tend - tbegin) / CountPerSec
@@ -57,16 +112,22 @@ program main
 
     open(15, file='aspherity_parameter.txt', status='replace')
 
-    write(15,*)'# aspherity parameter is:'
-    write (15,*)as
+    write(15,*)'# aspherity parameter'
+    write(15,*)'# delta is:'
+    write (15,*)ave(1)
+    write(15,*)'# sigma is:'
+    write (15,*)ave(2)
     close(15)
     open(17, file='log.as_param', status='replace')
     write (17,*)'read time is ',tread,'sec'
     write (17,*)'calclation time is ',tcalc,'sec'
     close(17)
+    print *,'# aspherity parameter'
+    print *,'# delta is:'
+    print *,ave(1)
+    print *,'# sigma is:'
+    print *,ave(2)
     
-    print*, '# AS param is:'
-    print*, as
 
 
 end program main
