c577f68c4b62275f43f537940de003f37468d099
goto-sh-at-Mtbys <goto-sh@cheng.es.osaka-u.ac.jp>
2023-02-13 15:55:47 +0900

    update

--- molecular-bond/prm_list.f90
+++
@@ -1,8 +0,0 @@
-module prm_list
-    implicit none 
-    integer nfreq, nparticle, natom, nmol, nstep, nframe
-    double precision dt, box_l
-    character(50) filename
-    double precision, allocatable :: pos(:,:,:) 
-    integer, parameter :: skip = 9
-end module prm_list 
